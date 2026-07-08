b1b23ae28d1537fec58cf045ea7e357d72c875cf
Sara Abu Arra <saraabuarra9@gmail.com>
2026-07-08 16:57:39 +0300

    Update steering_test.ino

--- esp32/steering_test/steering_test.ino
+++ esp32/steering_test/steering_test.ino
@@ -1,1 +1,54 @@
+#include <ESP32Servo.h>
 
+Servo steeringServo;
+
+#define SERVO_PIN 13
+
+// عدلي هاي القيم حسب معايرة سيارتك
+int CENTER = 70;
+int LEFT = 10;
+int RIGHT = 130;
+
+void setup() {
+
+  steeringServo.attach(SERVO_PIN);
+
+  // بداية: العجلات مستقيمة
+  steeringServo.write(CENTER);
+
+  delay(1000);
+}
+
+
+void loop() {
+
+  // مثال حركة اختبار
+  turnLeft();
+  delay(1000);
+
+  goStraight();
+  delay(1000);
+
+  turnRight();
+  delay(1000);
+
+  goStraight();
+  delay(1000);
+}
+
+
+// -------- Steering Functions --------
+
+void turnLeft() {
+  steeringServo.write(LEFT);
+}
+
+
+void turnRight() {
+  steeringServo.write(RIGHT);
+}
+
+
+void goStraight() {
+  steeringServo.write(CENTER);
+}
